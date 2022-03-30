21f2580cf2166226e9b18442efceecdef4bdc3a2
anton shabanov <89005421555@mail.ru>
2022-03-30 09:22:44 +0300

    reformat code

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,18 +2,17 @@
 public class BonusMilesService {
 
 
-public int calculate(int cost) {
+    public int calculate(int cost) {
 
-    int ticket = 10_000;
-    int rubles = 20;
+        int ticket = 10_000;
+        int rubles = 20;
 
-    int miles = (ticket / rubles);
+        int miles = (ticket / rubles);
 
-    return miles;
+        return miles;
 
 
-}
-
+    }
 
 
 }
